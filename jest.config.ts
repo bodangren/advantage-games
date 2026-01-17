b11fdab8ab6d691dc22d28a69faeac60a59fd9e2
Daniel Bo <bodangren@gmail.com>
2026-01-17 08:26:28 +0800

    feat(castle-defense): complete asset integration and finalize track artifacts

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(konva|react-konva)/)'
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
